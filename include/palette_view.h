0fa380b35dbe274c79a569aee5f07b82c2e93928
DmitryKurtaev <dmitry.kurtaev@gmail.com>
2016-06-25 21:35:46 +0300

    Replaced circle palette to rectange. Implemented Hue,Sat. rect drawing

--- include/palette_view.h
+++ include/palette_view.h
@@ -1,7 +1,10 @@
 #ifndef INCLUDE_PALETTE_VIEW_H_
 #define INCLUDE_PALETTE_VIEW_H_
 
+#include <stdint.h>
+
 #include "include/glview.h"
+#include "include/layout.h"
 
 class PaletteView : public GLView {
  public:
@@ -10,19 +13,34 @@ class PaletteView : public GLView {
   virtual void Display();
  
  private:
-  static const int kViewHeight = 100;
-  static const int kViewWidth = 200;
-  static const int kColorCircleRadius = 50;
+  static const int kViewHeight = 300;
+  static const int kViewWidth = 500;
+  // Layout parameters.
+  //  0.1      0.7
+  // +------------------+
+  // | +--------+ +---+ | 0.9
+  // | |   HS   | | V | |
+  // | +--------+ +---+ | 0.1
+  // +------------------+
+  //             0.8 0.9
+  static const float kHSPaletteTop = 0.9f;
+  static const float kHSPaletteLeft = 0.1f;
+  static const float kHSPaletteRight = 0.7f;
+  static const float kHSPaletteBottom = 0.1f;
+  static const float kVPaletteTop = 0.9f;
+  static const float kVPaletteLeft = 0.8f;
+  static const float kVPaletteRight = 0.9f;
+  static const float kVPaletteBottom = 0.1f;
 
-  void BuildColorCircle();
+  // h, s, v in [0, 1]
+  // r, g, b in [0, 255]
+  void HSVtoRGB(float h, float s, float v, uint8_t* colors_array_offset_);
 
-  void RGBtoHSV(float r, float g, float b, float* h, float* s, float* v);
-
-  void HSVtoRGB(float h, float s, float v, float* r, float* g, float* b);
-
+  Layout layout_;
   int* points_array_;
-  unsigned n_color_circle_points_;
-  float* colors_array_;
+  unsigned n_color_points_;
+  uint8_t* colors_array_;
+  uint8_t selected_solor_[3];
 };
 
 #endif  // INCLUDE_PALETTE_VIEW_H_
--- src/palette_view.cpp
+++ src/palette_view.cpp
@@ -1,39 +1,57 @@
 #include "include/palette_view.h"
 
 #include <math.h>
-#include <iostream>
+#include <algorithm>
 
 #include <GL/freeglut.h>
 
 PaletteView::PaletteView()
   : GLView(kViewWidth, kViewHeight, "Select color") {
-  BuildColorCircle();
+  const int hs_palette_top = kHSPaletteTop * kViewHeight;
+  const int hs_palette_left = kHSPaletteLeft * kViewWidth;
+  const int hs_palette_right = kHSPaletteRight * kViewWidth;
+  const int hs_palette_bottom = kHSPaletteBottom * kViewHeight;
+  const int hs_palette_width = (hs_palette_right - hs_palette_left + 1);
+  const int hs_palette_height = (hs_palette_top - hs_palette_bottom + 1);
+
+  n_color_points_ = hs_palette_width * hs_palette_height;
+  points_array_ = new int[n_color_points_ * 2];
+  colors_array_ = new uint8_t[n_color_points_ * 3];
+
+  int* points_array_offset_ = points_array_;
+  uint8_t* colors_array_offset_ = colors_array_;
+  for (int y = hs_palette_bottom; y <= hs_palette_top; ++y) {
+    for (int x = hs_palette_left; x <= hs_palette_right; ++x) {
+      points_array_offset_[0] = x;
+      points_array_offset_[1] = y;
+      HSVtoRGB(static_cast<float>(x - hs_palette_left) / hs_palette_width,
+               static_cast<float>(y - hs_palette_bottom) / hs_palette_height,
+               1.0f, colors_array_offset_);
+      points_array_offset_ += 2;
+      colors_array_offset_ += 3;
+    }
+  }
 }
 
-
 void PaletteView::Display() {
-  static const float kColorCircleRadius = 30;
-
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glMatrixMode(GL_PROJECTION);
   glPushMatrix();
   glLoadIdentity();
-  glOrtho(-kViewWidth / 2, kViewWidth / 2, -kViewHeight / 2, kViewHeight / 2,
-          0, 1);
+  glOrtho(0, kViewWidth, 0, kViewHeight, 0, 1);
 
   glMatrixMode(GL_MODELVIEW);
   glPushMatrix();
   glLoadIdentity();
 
-  glBegin(GL_POINTS);
-  float r, g, b;
-  for (unsigned i = 0; i < n_color_circle_points_; ++i) {
-    glColor3fv(colors_array_ + i * 3);
-    glVertex2iv(points_array_ + i * 2);
-  }
-  glEnd();
+  glEnableClientState(GL_COLOR_ARRAY);
+  glColorPointer(3, GL_UNSIGNED_BYTE, 0, colors_array_);
+
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glVertexPointer(2, GL_INT, 0, points_array_);
+  glDrawArrays(GL_POINTS, 0, n_color_points_);
 
   glMatrixMode(GL_MODELVIEW);
   glPopMatrix();
@@ -44,29 +62,9 @@ void PaletteView::Display() {
   glutSwapBuffers();
 }
 
-void PaletteView::RGBtoHSV(float r, float g, float b, float* h, float* s,
-                           float* v) {
-  static const float kRatio = 60.0f / 360.0f;
-  float max_rgb = std::max(std::max(r, g), b);
-  float min_rgb = std::min(std::min(r, g), b);
-  *h = 0;
-  if (max_rgb != min_rgb) {
-    if (max_rgb == r) {
-      *h = kRatio * (g - b) / (max_rgb - min_rgb) + (g >= b ? 0 : 1);
-    } else if (max_rgb == g) {
-      *h = kRatio * ((b - r) / (max_rgb - min_rgb) + 2);
-    } else {
-      *h = kRatio * ((r - g) / (max_rgb - min_rgb) + 4);
-    }
-  }
-  *s = (max_rgb != 0 ? 1 - min_rgb / max_rgb : 0);
-  *v = max_rgb;
-}
-
-void PaletteView::HSVtoRGB(float h, float s, float v, float* r, float* g,
-                           float* b) {
+void PaletteView::HSVtoRGB(float h, float s, float v,
+                           uint8_t* colors_array_offset_) {
   v *= 100.0f;
-  int idx = static_cast<int>(h * 6) % 6;
   float v_min = (1.0f - s) * v;
   float a = (static_cast<int>(h * 360.0f) % 60) * (v - v_min) / 60.0f;
   float v_inc = v_min + a;
@@ -74,79 +72,8 @@ void PaletteView::HSVtoRGB(float h, float s, float v, float* r, float* g,
   float rs[] = {v, v_dec, v_min, v_min, v_inc, v};
   float gs[] = {v_inc, v, v, v_dec, v_min, v_min};
   float bs[] = {v_min, v_min, v_inc, v, v, v_dec};
-  *r = rs[idx] / 255.0f;
-  *g = gs[idx] / 255.0f;
-  *b = bs[idx] / 255.0f;
+  int idx = static_cast<int>(h * 6) % 6;
+  colors_array_offset_[0] = std::max(0.0f, std::min(rs[idx] * 2.55f, 255.0f));
+  colors_array_offset_[1] = std::max(0.0f, std::min(gs[idx] * 2.55f, 255.0f));
+  colors_array_offset_[2] = std::max(0.0f, std::min(bs[idx] * 2.55f, 255.0f));
 }
-
-void PaletteView::BuildColorCircle() {
-  int x = kColorCircleRadius;
-  int y = 0;
-  int* right_xs = new int[kColorCircleRadius + 1];
-  right_xs[0] = x;
-  n_color_circle_points_ = 4 * kColorCircleRadius + 1;
-  do {
-    int term = kColorCircleRadius * kColorCircleRadius - y * y - x * x;
-    int diff_up = abs(term - 2 * y - 1);
-    int diff_left = abs(term + 2 * x - 1);
-    int diff_up_left = abs(term + 2 * x - 2 * y - 2);
-    if (diff_left <= diff_up) {
-      --x;
-      if (diff_up_left <= diff_left) {
-        right_xs[++y] = x;
-        n_color_circle_points_ += 4 * x;
-      }
-    } else {
-      if (diff_up_left <= diff_up) {
-        --x;
-      }
-      right_xs[++y] = x;
-      n_color_circle_points_ += 4 * x;
-    }
-  } while (x != 0);
-
-  points_array_ = new int[2 * n_color_circle_points_];
-  points_array_[0] = 0;
-  points_array_[1] = 0;
-  unsigned idx = 2;
-  for (int x = 1; x <= right_xs[0]; ++x) {
-    points_array_[idx++] = x;
-    points_array_[idx++] = 0;
-    points_array_[idx++] = -x;
-    points_array_[idx++] = 0;    
-
-    points_array_[idx++] = 0;
-    points_array_[idx++] = x;    
-    points_array_[idx++] = 0;
-    points_array_[idx++] = -x;
-  }
-
-  for (int y = 1; y <= kColorCircleRadius; ++y) {
-    for (int x = 1; x <= right_xs[y]; ++x) {
-      points_array_[idx++] = x;
-      points_array_[idx++] = y;
-      points_array_[idx++] = -x;
-      points_array_[idx++] = y;
-      points_array_[idx++] = x;
-      points_array_[idx++] = -y;
-      points_array_[idx++] = -x;
-      points_array_[idx++] = -y;
-    }
-  }
-  delete[] right_xs;
-
-  if (idx != 2 * n_color_circle_points_) {
-    std::cout << "Mistake in PaletteView::BuildColorCircle()" << std::endl;
-  }
-
-  colors_array_ = new float[n_color_circle_points_ * 3];
-  for (unsigned i = 0; i < n_color_circle_points_; ++i) {
-    float x = points_array_[i * 2];
-    float y = points_array_[i * 2 + 1];
-    float h = (atan2(y, x) + M_PI) * 0.5 / M_PI;
-    float s = sqrt(x * x + y * y);
-    HSVtoRGB(h, s, 1.0f, colors_array_ + i * 3, colors_array_ + i * 3 + 1,
-             colors_array_ + i * 3 + 2);
-  }
-}
- 
