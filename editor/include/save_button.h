2a6473c85b204d33e22dd34e64d2ccbb75f6e0dc
dkurtaev <dmitry.kurtaev@gmail.com>
2016-07-17 07:33:26 +0300

    Saving by last command path

--- editor/include/save_button.h
+++ editor/include/save_button.h
@@ -11,13 +11,18 @@ class SaveButton : public Button {
  public:
   SaveButton(const Icosphere* icosphere, ConsoleView* console_view);
 
+  // Tip console command or save by last path. New command overwrites last path.
   virtual void MouseFunc(int button, int state, int x, int y);
 
+  // Parse console for "save" command.
   virtual void DoEvents();
+
+  void Save();
 
  private:
   const Icosphere* icosphere_;
   ConsoleView* console_view_;
+  std::string last_path_;
 };
 
 #endif  // EDITOR_INCLUDE_SAVE_BUTTON_H_
--- editor/src/save_button.cpp
+++ editor/src/save_button.cpp
@@ -5,30 +5,42 @@
 #include <string>
 
 SaveButton::SaveButton(const Icosphere* icosphere, ConsoleView* console_view)
-  : Button("Save"), icosphere_(icosphere), console_view_(console_view) {}
+  : Button("Save"), icosphere_(icosphere), console_view_(console_view),
+    last_path_("") {}
 
 void SaveButton::MouseFunc(int button, int state, int x, int y) {
   if (!state) {
-    console_view_->Write("Enter command \"save\" [<path>]");
-    // Reset current command, if exists.
-    std::string command;
-    console_view_->ProcessCommand(&command);
+    Save();
   }
 }
 
 void SaveButton::DoEvents() {
-  static const std::string kCommandPrefix = "save ";
+  static const std::string kCommandPrefix = "save";
 
   std::string command;
   console_view_->ProcessCommand(&command);
   if (command != "") {
-    int idx = command.find(kCommandPrefix);
+    int idx = command.find(kCommandPrefix);  // Check prefix.
     if (idx != -1) {
-      command = command.substr(idx + kCommandPrefix.size());
-      icosphere_->Save(command);
-      console_view_->Write("Model saved as " + command);
+      if (command != kCommandPrefix) {
+        command = command.substr(idx + (kCommandPrefix + ' ').size());
+        last_path_ = command;
+      }
+      Save();
     } else {
       console_view_->ReturnCommand(command);
     }
+  }
+}
+
+void SaveButton::Save() {
+  if (last_path_ != "") {
+    icosphere_->Save(last_path_);
+    console_view_->Write("Model saved as " + last_path_);
+  } else {
+    console_view_->Write("Enter command \"save\" [<path>]");
+    // Reset current command, if exists.
+    std::string command;
+    console_view_->ProcessCommand(&command);
   }
 }
